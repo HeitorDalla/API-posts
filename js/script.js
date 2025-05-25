111811975502d43129048a0ab267ea2e96ba5a54
HeitorDalla <88400131+HeitorDalla@users.noreply.github.com>
2025-05-25 12:31:36 -0300

    Finalizado as requisições com api

--- js/script.js
+++ js/script.js
@@ -47,3 +47,80 @@ async function pegarPosts () {
         console.error(error)
     }
 }
+
+// Função para pegar cada post individual
+async function pegarPost (id) {
+    // Trabalhas com os dois request assíncronos
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${endpoint}/${id}`),
+        fetch(`${endpoint}/${id}/comments`)
+    ]);
+
+    const dataPost = await responsePost.json();
+    const dataComments = await responseComments.json();
+
+    loading.classList.add("hide");
+    paginaPost.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    title.innerText = dataPost.body;
+
+    containerPost.appendChild(title);
+    containerPost.appendChild(body);
+
+    dataComments.map((comment) => {
+        criarComentario(comment);
+    })
+}
+
+// Funcao para fazer um comentario no post
+async function postComentario (comentario) {
+    const response = await fetch(`${endpoint}/${postId}/comments`, {
+        method: "POST", // Método utilizado para enviar para eu requisição
+        body: comentario, // Conteudo
+        headers: { // Qual tipo de dados eu estou me comunicando com a API
+            "Content-type": "application/json"
+        }
+    })
+
+    const data = await response.json();
+    criarComentario(data);
+}
+
+// Função para criar comentario de cada post
+function criarComentario (comment) {
+    const div = document.createElement("div");
+    const email = document.createElement("h2");
+    const body = document.createElement("p");
+
+    email.innerText = comment.email;
+    body.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(body);
+
+    containerComentarios.appendChild(div);
+}
+
+if (!postId) {
+    pegarPosts();
+} else {
+    pegarPost(postId);
+
+    // Adicionar evento para comentario
+    formComentarios.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let comentario = {
+            email: email.value,
+            body: body.value
+        }
+
+        comentario = JSON.stringify(comentario);
+
+        postComentario(comentario);
+    })
+}
